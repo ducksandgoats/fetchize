1f5ae774db80d43e54d1f2fe5ae304aca7ef6957
ducksandgoats <mrtraderjoseph@gmail.com>
2024-08-30 23:21:18 -0500

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+import makeHTTP from "./http-protocol"
+import makeGemini from "./gemini-protocol"
+import makeGopher from "./gopher-protocol"
+import makeBTFetch from "./bt-protocol"
+import makeIPFSFetch from "./ipfs-protocol"
+import makeHyperFetch from "./hyper-protocol"
+import makeOnion from "./tor-protocol"
+import makeGarlic from "./iip-protocol"
+import makeIndex from "./oui-protocol"
+
+export {makeHTTP, makeGemini, makeGopher, makeBTFetch, makeIPFSFetch, makeHyperFetch, makeOnion, makeGarlic, makeIndex}
