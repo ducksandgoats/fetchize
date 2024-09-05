74b9637bc09aca3313a87a7fd0730f32b8dba8e0
ducksandgoats <mrtraderjoseph@gmail.com>
2024-09-04 21:56:22 -0500

    update package

--- ipfs-protocol.js
+++ ipfs-protocol.js
@@ -5,7 +5,7 @@ export default async function makeIPFSFetch (opts = {}) {
     const { gossipsub } = await import('@chainsafe/libp2p-gossipsub')
     const {identify} = await import('@libp2p/identify')
     const {default: parseRange} = await import('range-parser')
-    const {createLibp2p} = await import('libp2p')
+    const { circuitRelayTransport, circuitRelayServer } = await import('@libp2p/circuit-relay-v2')
     const {default: mime} = await import('mime')
     const { Readable } = await import('streamx')
     const path = await import('path')
@@ -26,8 +26,7 @@ export default async function makeIPFSFetch (opts = {}) {
       'Access-Control-Request-Headers': '*'
     }
     
-    const app = await (async () => { if (finalOpts.helia) { return finalOpts.helia; } else {const {createHelia} = await import('helia');const {FsDatastore} = await import('datastore-fs');const {FsBlockstore} = await import('blockstore-fs');return await createHelia({blockstore: new FsBlockstore(repo), datastore: new FsDatastore(repo), libp2p: await createLibp2p({services: {dht: kadDHT(), pubsub: gossipsub(), identify: identify()}})});} })()
-    // console.log(Object.keys(app), app)
+    const app = await (async () => { if (finalOpts.helia) { return finalOpts.helia; } else {const {createHelia} = await import('helia');const {FsDatastore} = await import('datastore-fs');const {FsBlockstore} = await import('blockstore-fs');return await createHelia({blockstore: new FsBlockstore(repo), datastore: new FsDatastore(repo), libp2p: {transports: [circuitRelayTransport({discoverRelays: 0,reservationConcurrency: 1})], services: {dht: kadDHT(), pubsub: gossipsub(), identify: identify(), relay: circuitRelayServer({hopTimeout: 30 * 1000,advertise: true,reservations: {maxReservations: 15,reservationClearInterval: 300 * 1000,applyDefaultLimit: true,defaultDurationLimit: 2 * 60 * 1000,defaultDataLimit: BigInt(2 << 7),maxInboundHopStreams: 32,maxOutboundHopStreams: 64}})}}});} })()
     const fileSystem = await (async () => {const {unixfs} = await import('@helia/unixfs');return unixfs(app);})()
     if(!await fs.pathExists(repo)){
       await fs.ensureDir(repo)
