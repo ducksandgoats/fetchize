4ed4fc1e0d4713474482d375c5bdb3eb254d3b6b
ducksandgoats <mrtraderjoseph@gmail.com>
2025-02-16 01:07:39 -0600

    update

--- index.js
+++ index.js
@@ -14,37 +14,37 @@ export default async function(fsPath, opts = {}){
     const sdk = await (async () => {const SDK = await import('hyper-sdk');const sdk = await SDK.create(appOpts.hyper);return sdk;})()
 
     if(opts.bt){
-        const {default: createBTHandler} = await import('./bt-protocol.js')
+        const {default: createBTHandler} = await import('./proto/bt-protocol.js')
         const { handler: btHandler, close: closeBT } = await createBTHandler({...appOpts.bt, torrentz})
         closer.push(closeBT)
         obj.bt = btHandler
     }
     if(opts.ipfs){
-        const {default: createIPFSHandler} = await import('./ipfs-protocol.js')
+        const {default: createIPFSHandler} = await import('./proto/ipfs-protocol.js')
         const { handler: ipfsHandler, close: closeIPFS } = await createIPFSHandler({...appOpts.ipfs, helia})
         closer.push(closeIPFS)
         obj.ipfs = ipfsHandler
     }
     if(opts.hyper){
-        const {default: createHyperHandler} = await import('./hyper-protocol.js')
+        const {default: createHyperHandler} = await import('./proto/hyper-protocol.js')
         const { handler: hyperHandler, close: closeHyper } = await createHyperHandler({...hyper, sdk})
         closer.push(closeHyper)
         obj.hyper = hyperHandler
     }
     if(opts.msg){
-        const {default: createMsgHandler} = await import('./msg-protocol.js')
+        const {default: createMsgHandler} = await import('./proto/msg-protocol.js')
         const { handler: msgHandler, close: closeMsg } = await createMsgHandler({...appOpts.bt, torrentz})
         closer.push(closeMsg)
         obj.msg = msgHandler
     }
     if(opts.pubsub){
-        const {default: createPubsubHandler} = await import('./pubsub-protocol.js')
+        const {default: createPubsubHandler} = await import('./proto/pubsub-protocol.js')
         const { handler: pubsubHandler, close: closePubsub } = await createPubsubHandler({...ipfs, helia})
         closer.push(closePubsub)
         obj.pubsub = pubsubHandler
     }
     if(opts.topic){
-        const {default: createTopicHandler} = await import('./topic-protocol.js')
+        const {default: createTopicHandler} = await import('./proto/topic-protocol.js')
         const { handler: topicHandler, close: closeTopic } = await createTopicHandler({...hyper, sdk}, session)
         closer.push(closeTopic)
         obj.topic = topicHandler
